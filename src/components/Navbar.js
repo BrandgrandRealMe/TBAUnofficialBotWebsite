9bdbb1768aac005a7263f1a288abb7fb2fc2bcf1
BrandgrandRealMe <brandon@brandgrand.tech>
2024-05-13 13:06:35 -0500

    Added new Favicon & added logo to header

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,19 @@
 import Link from "next/link";
+import Image from "next/image";
+import Logo from "../utils/images/TBAgold.png";
 
 const Navbar = () => {
   return (
     <div>
       <navbar className="navbar">
         <nav className="navbar-header">
+        <Image
+                    src={Logo}
+                    width={100}
+                    height={100}
+                    alt="TBA logo but gold"
+                    className="Logo circle"
+                  />{" "}
           <ul>
             <li>
               <Link className="h-color" href="/">
