c9ac0645c2929bb46c48d62e3036696ec89a9b33
BrandgrandRealMe <brandon@brandgrand.tech>
2024-05-13 14:40:54 -0500

    More fixes and testin'

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,12 +7,12 @@ const Navbar = () => {
     <div>
       <navbar className="navbar">
         <nav className="navbar-header">
-        <Image
+          <ul class="navbarTitle">
+          <Image
                     src={Logo}
                     alt="TBA logo but gold"
                     className="Logo circle"
                   />
-          <ul>
             <li>
               <Link className="h-color" href="/">
                 TBAUnofficial
